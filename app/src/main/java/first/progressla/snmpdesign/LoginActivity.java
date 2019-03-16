ceb5e970f4ef558bae60f78946ba4f47ab4b5572
Yasmini19 <aldrey.twox@gmail.com>
2019-03-16 13:58:20 +0700

    SplashScreen

--- app/src/main/java/first/progressla/snmpdesign/LoginActivity.java
+++ app/src/main/java/first/progressla/snmpdesign/LoginActivity.java
@@ -1,13 +1,28 @@
 package first.progressla.snmpdesign;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        rellay1 = findViewById(R.id.rellay1);
+        handler.postDelayed(runnable, 2000);
     }
 }
